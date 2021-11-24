23f07dfc934a2148260c225c9be1656a5e8eb321
zhifuzhu4 <zhifuzhu4@gmail.com>
2021-11-23 22:38:30 -0600

    Fix bug in reverse_integer

--- easy/reverse_integer.py
+++ easy/reverse_integer.py
@@ -40,6 +40,5 @@ if bool_val == True, ans <- 'val 1'
 class Solution:
     def reverse(self, x: int) -> int:
         sign = 1 if x > 0 else -1
-        rst = sign * int(str(x)[::-1])
-        return rst if -2**31 < rst < 2**31-1 else 0
-
+        rst = sign * int(str(abs(x))[::-1])
+        return rst if -2**31 < rst < 2**31 - 1 else 0
