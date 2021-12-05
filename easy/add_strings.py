fce63692036dfe6bc87e2095ee18ba742863feeb
Zhifu Zhu <zhifuzhu4@gmail.com>
2021-12-05 10:11:10 -0600

    add strings

---
+++ easy/add_strings.py
@@ -0,0 +1,40 @@
+"""
+415. Add Strings
+Given two non-negative integers, num1 and num2 represented as string,
+return the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger).
+You must also not convert the inputs to integers directly.
+
+Example 1:
+Input: num1 = "11", num2 = "123"
+Output: "134"
+
+Example 2:
+Input: num1 = "456", num2 = "77"
+Output: "533"
+
+Example 3:
+Input: num1 = "0", num2 = "0"
+Output: "0"
+
+Constraints:
+1 <= num1.length, num2.length <= 104
+num1 and num2 consist of only digits.
+num1 and num2 don't have any leading zeros except for the zero itself.
+"""
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        def str2int(num):
+            num_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+                        '6': 6, '7': 7, '8': 8, '9': 9, '10': 10}
+            rst = 0
+            for d in num:
+                rst = rst * 10 + num_dict[d]
+            return rst
+        return str(str2int(num1) + str2int(num2))
+
+
+
