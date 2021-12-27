c86bea5fdd62c5fb1796437fd8dafd3170fbb560
Zhifu Zhu <zhifuzhu4@gmail.com>
2021-12-27 16:55:18 -0600

    valid parentheses

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -33,11 +33,31 @@ Constraints:
 s consists of parentheses only '()[]{}'.
 """
 
-# TODO: solution with stack
-
 
 class Solution:
     def validParentheses(self, s: str) -> bool:
+        # Time Limit Exceeded
+        # Replace function will replace only one set {} at a time.
+        # Replace itself takes O(N) time because it will have to search the entire string.
+        # On top of this, we have while loop which runs for O(N/2) times.
+        # So O(N) * O(N/2) results in O(N^2)
+        if len(s) % 2:
+            return False
+
         while "()" in s or "[]" in s or "{}" in s:
             s = s.replace("()", "").replace("[]", "").replace("{}", "")
         return s == ""
+
+    def validParentheses2(self, s: str) -> bool:
+        # 1. if it's the left bracket then we append it to the stack
+        # 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket),
+        # or the left bracket doesn't match
+        # 3. finally check if the stack still contains unmatched left bracket
+        stack = []
+        d = {'(': ')', '[': ']', '{': '}'}
+        for x in s:
+            if x in d:  # 1
+                stack.append(x)
+            elif len(stack) == 0 or d[stack.pop()] != x:  # 2
+                return False
+        return len(stack) == 0  # 3
