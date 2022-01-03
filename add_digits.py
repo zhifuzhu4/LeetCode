6cda254f6a969d4fdad19221f2ed0228893d19dd
Zhifu Zhu <zhifuzhu4@gmail.com>
2022-01-02 23:06:29 -0600

    add digits

--- add_digits.py
+++ add_digits.py
@@ -19,10 +19,30 @@ Constraints:
 0 <= num <= 2**31 - 1
 
 Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+Note:
+N=(a[0] * 1 + a[1] * 10 + ...a[n] * 10 ^n),and a[0]...a[n] are all between [0,9]
+we set M = a[0] + a[1] + ..a[n]
+and another truth is that:
+1 % 9 = 1
+10 % 9 = 1
+100 % 9 = 1
+so N % 9 = a[0] + a[1] + ..a[n]
+
+means N % 9 = M
+so N = M (% 9)
+as 9 % 9 = 0,so we can make (n - 1) % 9 + 1 to help us solve the problem when n is 9.as N is 9, ( 9 - 1) % 9 + 1 = 9
+
+also read the analysis from: https://blog.csdn.net/fuxuemingzhu/article/details/49161129
 """
 
 class Solution:
     def addDigits(self, num: int) -> int:
-        while num > 9:
+        while num >= 10:
             num = sum(int(x) for x in str(num))
         return num
+
+    def addDigits2(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return (num-1) % 9 + 1
