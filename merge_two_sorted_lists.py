8fed63810f3273b15832abd529496874e101351d
Zhifu Zhu <zhifuzhu4@gmail.com>
2022-01-18 14:33:27 -0600

    merge two sorted lists

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -37,7 +37,8 @@ class ListNode:
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         # iteratively
-        head = cur = ListNode(0)
+        head = ListNode(0)
+        cur = head
         while list1 and list2:
             if list1.val < list2.val:
                 cur.next = list1
