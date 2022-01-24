2c386adb1d0a135b5b02b5ea871bb1fe216c904e
Zhifu Zhu <zhifuzhu4@gmail.com>
2022-01-23 21:22:16 -0600

    minimum window substring

---
+++ minimum_window_substring.py
@@ -0,0 +1,89 @@
+"""
+76. Minimum Window Substring
+Given two strings s and t of lengths m and n respectively,
+return the minimum window substring of s such that every character in t (including duplicates)
+is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+Example 2:
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+
+Example 3:
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+
+Constraints:
+m == s.length
+n == t.length
+1 <= m, n <= 10**5
+s and t consist of uppercase and lowercase English letters.
+
+Note: two pointers
+1. Use two pointers: start and end to represent a window.
+2. Move end to find a valid window (expand).
+3. When a valid window is found, move start to find a smaller window (shrink).
+"""
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # Count the frequencies for chars in t
+        hash_map = {}
+        for c in t:
+            hash_map[c] = hash_map.get(c, 0) + 1
+
+        start = end = 0  # start and end are not keywords of python
+
+        # If the minimal length doesn't change, it means there's no valid window
+        min_window_length = len(s) + 1
+
+        # Start point of the minimal window
+        min_window_start = 0
+
+        # Works as a counter of how many chars still need to be included in a window
+        num_of_chars_to_be_included = len(t)
+
+        while end < len(s):
+            # If the current char is desired
+            if s[end] in hash_map:
+                # Then we decrease the counter, if this char is a "must-have" now, in a sense of critical value
+                if hash_map[s[end]] > 0:
+                    num_of_chars_to_be_included -= 1
+                # And we decrease the hash_map value
+                hash_map[s[end]] -= 1
+
+            # If the current window has all the desired chars
+            while num_of_chars_to_be_included == 0:
+                # See if this window is smaller
+                if end - start + 1 < min_window_length:
+                    min_window_length = end - start + 1
+                    min_window_start = start
+
+                # if s[start] is desired, we need to update the hash_map value and the counter
+                if s[start] in hash_map:
+                    hash_map[s[start]] += 1
+                    # Still, update the counter only if the current char is "critical"
+                    if hash_map[s[start]] > 0:
+                        num_of_chars_to_be_included += 1
+
+                # Move start forward to find a smaller window
+                start += 1
+
+            # Move end forward to find another valid window
+            end += 1
+
+        if min_window_length == len(s) + 1:
+            return ""
+        else:
+            return s[min_window_start:min_window_start + min_window_length]
