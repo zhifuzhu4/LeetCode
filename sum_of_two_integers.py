e58df73f878f7fc6e2363f58037332f0ab8fa22f
Zhifu Zhu <zhifuzhu4@gmail.com>
2022-01-23 22:30:57 -0600

    sum of two integers

--- sum_of_two_integers.py
+++ sum_of_two_integers.py
@@ -14,3 +14,21 @@ Constraints:
 -1000 <= a, b <= 1000
 """
 
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        """
+        https://leetcode.com/problems/sum-of-two-integers/discuss/489210/Read-this-if-you-want-to-learn-about-masks
+        """
+        # 32 bit mask in hexadecimal
+        mask = 0xffffffff
+
+        # works both as while loop and single value check
+        while (b & mask) > 0:
+            carry = (a & b) << 1
+            a = (a ^ b)
+            b = carry
+
+        # handles overflow
+        return (a & mask) if b > 0 else a
+
