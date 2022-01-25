f7fb5c92a897d30712225534a208ee5b76650460
Zhifu Zhu <zhifuzhu4@gmail.com>
2022-01-24 22:23:41 -0600

    reverse bits

--- reverse_bits.py
+++ reverse_bits.py
@@ -7,6 +7,11 @@ Output: 964176192
 Explanation:
 43261596 represented in binary as 00000010100101000001111010011100,
 return 964176192 represented in binary as 00111001011110000010100101000000.
+
+Constraints:
+The input must be a binary string of length 32
+
+Follow up: If this function is called many times, how would you optimize it?
 """
 
 
@@ -19,7 +24,8 @@ class Solution:
 
     def reverseBits2(self, n: int) -> int:
         ans = 0
-        for i in range(32):
+        for _ in range(32):
+            # the for loop cannot be replaced by while n since the output has to be a string of length 32.
             ans = (ans << 1) + (n & 1)
             n >>= 1
         return ans
