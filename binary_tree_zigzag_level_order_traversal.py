76c2a6292b8613933979bac8bb644702e94c9fe0
Zhifu Zhu <zhifuzhu4@gmail.com>
2022-02-04 22:07:57 -0600

    binary tree zigzag level order traversal

---
+++ binary_tree_zigzag_level_order_traversal.py
@@ -0,0 +1,50 @@
+"""
+103. Binary Tree Zigzag Level Order Traversal
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values.
+(i.e., from left to right, then right to left for the next level and alternate between).
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+
+Example 2:
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+
+# TODO: study other solutions
+"""
+
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        res, level = [], [root]
+        while level:
+            res.append([node.val for node in level])
+            temp = []
+            for node in level:
+                temp.extend([node.left, node.right])
+            level = [leaf for leaf in temp if leaf]
+        for i, v in enumerate(res):
+            if i % 2 != 0:
+                res[i] = res[i][::-1]
+        return res
