74f8210d706efd829e0c168ba1db3c5ba91aa0ae
Zhifu Zhu <zhifuzhu4@gmail.com>
2022-02-06 22:59:11 -0600

    binary tree paths

---
+++ binary_tree_paths.py
@@ -0,0 +1,76 @@
+"""
+257. Binary Tree Paths
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+A leaf is a node with no children.
+
+Example 1:
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+
+Example 2:
+Input: root = [1]
+Output: ["1"]
+
+Constraints:
+The number of nodes in the tree is in the range [1, 100].
+-100 <= Node.val <= 100
+"""
+
+from typing import Optional, List
+import collections
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # DFS + stack
+        if not root:
+            return []
+        res, stack = [], [(root, "")]
+        while stack:
+            node, ls = stack.pop()
+            if not node.left and not node.right:
+                res.append(ls+str(node.val))
+            if node.right:
+                stack.append((node.right, ls+str(node.val)+"->"))
+            if node.left:
+                stack.append((node.left, ls+str(node.val)+"->"))
+        return res
+
+    def binaryTreePaths2(self, root: Optional[TreeNode]) -> List[str]:
+        # BFS + queue
+        if not root:
+            return []
+        res, queue = [], collections.deque([(root, "")])
+        while queue:
+            node, ls = queue.popleft()
+            if not node.left and not node.right:
+                res.append(ls+str(node.val))
+            if node.left:
+                queue.append((node.left, ls+str(node.val)+"->"))
+            if node.right:
+                queue.append((node.right, ls+str(node.val)+"->"))
+        return res
+
+    def binaryTreePaths3(self, root: Optional[TreeNode]) -> List[str]:
+        # DFS recursively
+        if not root:
+            return []
+        res = []
+        self.dfs(root, "", res)
+        return res
+
+    def dfs(self, root, ls, res):
+        if not root.left and not root.right:
+            res.append(ls+str(root.val))
+        if root.left:
+            self.dfs(root.left, ls+str(root.val)+"->", res)
+        if root.right:
+            self.dfs(root.right, ls+str(root.val)+"->", res)
